5ba2f8faf3bd14f2748df176ec05c5088aa19ee6
martypdx <marty@martypdx.com>
2017-01-14 10:44:34 -0800

    few comments on charts

--- app/src/components/charts/chart.js
+++ app/src/components/charts/chart.js
@@ -21,22 +21,11 @@ function controller($state) {
     this.selectedChart = 'all';
 
     this.updateChart = function() {
-
-        if (this.chartStyle === 'pie') {
-            if (this.selectedChore === 'all') {
-                $state.go('charts.allpie');
-            } else {
-                $state.go('charts.pie', {choreId: this.selectedChore});
-            }
-        } 
-
-        if (this.chartStyle === 'bar') {
-            if (this.selectedChore === 'all') {
-                $state.go('charts.allbar');
-            } else {
-                $state.go('charts.bar', {choreId: this.selectedChore});
-            }
-        }
-        
-    }
+        const style = this.chartStyle;
+        if (this.selectedChore === 'all') {
+            $state.go(`charts.all${style}`);
+        } else {
+            $state.go(`charts.${style}`, {choreId: this.selectedChore});
+        }        
+    };
 }
